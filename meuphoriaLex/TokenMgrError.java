e794b08159e66b901effcc1717d414bbc23af703
Gorodscy <fefow@me.com>
2014-04-08 20:02:49 -0300

    script para rodar testes criado para unix

--- meuphoriaLex/TokenMgrError.java
+++ meuphoriaLex/TokenMgrError.java
@@ -103,9 +103,9 @@ public class TokenMgrError extends Error
    * Note: You can customize the lexical error message by modifying this method.
    */
   protected static String LexicalError(boolean EOFSeen, int lexState, int errorLine, int errorColumn, String errorAfter, char curChar) {
-    return("Erro lexico na linha " +
-          errorLine + ", coluna " +
-          errorColumn + ".  Encontrou: " +
+    return("Lexical error at line " +
+          errorLine + ", column " +
+          errorColumn + ".  Encountered: " +
           (EOFSeen ? "<EOF> " : ("\"" + addEscapes(String.valueOf(curChar)) + "\"") + " (" + (int)curChar + "), ") +
           "after : \"" + addEscapes(errorAfter) + "\"");
   }
